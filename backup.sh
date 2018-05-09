9b73a6fc521fa5d251b1fea1cbfa7d6d529379a9
sheng <jrchens@126.com>
2018-05-09 15:23:00 +0800

    shell file (use the same filename)

--- backup.sh
+++ backup.sh
@@ -4,18 +4,18 @@
 # 30 23 * * * /usr/local/bin/backup.sh
 
 # #################################################################################
-DB_FILE="/usr/local/var/backup/jracd-db.sql.gz"
+DB_FILE="/usr/local/var/backup/jrry.sql.gz"
 
 if [ -f "$DB_FILE" ] ; then
 	rm -f "$DB_FILE"
 fi
-/usr/bin/mysqldump -uroot -h127.0.0.1 --add-locks=false --lock-tables=false --events=true jracd | /bin/gzip > $DB_FILE
+/usr/bin/mysqldump -uroot -h127.0.0.1 --add-locks=false --lock-tables=false --events=true jrry | /bin/gzip > $DB_FILE
 
 
 # #################################################################################
 UPLOAD_FILE_BASE_PATH="/usr/local/var/upload"
-UPLOAD_FILE_DIR="jracd"
-UPLOAD_BACKUP_FILE="/usr/local/var/backup/jracd-file.tar"
+UPLOAD_FILE_DIR="jrry"
+UPLOAD_BACKUP_FILE="/usr/local/var/backup/jrry.tar"
 
 if [ ! -f "$UPLOAD_BACKUP_FILE" ] ; then
 	/bin/tar cPf $UPLOAD_BACKUP_FILE -C $UPLOAD_FILE_BASE_PATH $UPLOAD_FILE_DIR
@@ -29,7 +29,7 @@ fi
 
 
 # #################################################################################
-OSS_BUCKET_DIR="jracd"
+OSS_BUCKET_DIR="jrry"
 
 /usr/local/jdk1.7.0_80/bin/java -jar /usr/local/bin/oss-upload.jar $OSS_BUCKET_DIR $DB_FILE "$UPLOAD_BACKUP_FILE".gz
 
